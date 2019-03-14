136a5a8d621f9247e94a918aa7e1d6b4dc7f4fd0
martin.scocco <martin.scocco@santexgroup.com>
2019-03-14 16:59:41 -0300

    Add login component with firebase

--- src/components/root/Root.component.jsx
+++ src/components/root/Root.component.jsx
@@ -5,6 +5,7 @@ import { Container } from 'reactstrap';
 // Local dependencies
 import Routes from '../../Routes.component';
 import { Header } from '../index';
+import Login from '../login/Login.component';
 
 // Styles
 import './Root.scss';
@@ -13,6 +14,7 @@ import './Root.scss';
 const Root = () => (
   <Container fluid className="root-wrapper">
     <Header />
+    <Login/>
     <div className="content">
       <Routes />
     </div>
